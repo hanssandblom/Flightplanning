fea55f8e7781f60b90329716e7d7cf94745e2ac6
CooBoo84 <kennie_svensson@hotmail.com>
2021-11-30 13:29:21 +0100

    feat(AirplaneEntity): add airplane variables

--- src/main/java/se/iths/flightplanning/entity/AirplaneEntity.java
+++ src/main/java/se/iths/flightplanning/entity/AirplaneEntity.java
@@ -1,4 +1,19 @@
 package se.iths.flightplanning.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class AirplaneEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String airplaneModel;
+    private int numberOfSeat;
+    private int numberOfPersonal;
+
 }
