0750bed94939a803351e5dde6753fa1c254fcb83
CooBoo84 <kennie_svensson@hotmail.com>
2021-11-30 13:25:11 +0100

    feat(UserEntity & Airplane): add user variables

---
+++ src/main/java/se/iths/flightplanning/entity/AirplaneEntity.java
@@ -0,0 +1,4 @@
+package se.iths.flightplanning.entity;
+
+public class AirplaneEntity {
+}
---
+++ src/main/java/se/iths/flightplanning/entity/UserEntity.java
@@ -0,0 +1,88 @@
+package se.iths.flightplanning.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String telephone;
+    private String username;
+    private String password;
+
+    public UserEntity(String firstName, String lastName, String email, String telephone, String username, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.telephone = telephone;
+        this.username = username;
+        this.password = password;
+    }
+
+    public UserEntity() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
