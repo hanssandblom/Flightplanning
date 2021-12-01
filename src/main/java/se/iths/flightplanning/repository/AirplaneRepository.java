a0a18b12ab3ff64042793806b670657731b8dd6b
CooBoo84 <kennie_svensson@hotmail.com>
2021-12-01 10:05:49 +0100

    feat(Airplane): add create and find all

---
+++ src/main/java/se/iths/flightplanning/repository/AirplaneRepository.java
@@ -0,0 +1,9 @@
+package se.iths.flightplanning.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import se.iths.flightplanning.entity.AirplaneEntity;
+
+@Repository
+public interface AirplaneRepository extends CrudRepository<AirplaneEntity, Long> {
+}
